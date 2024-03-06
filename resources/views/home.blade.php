cf33646082f05f117c5feb9947d53b2da949d213
ripoaayulga <rivoaayulga@gmail.com>
2024-03-06 20:24:10 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
     </head>
     <body class="antialiased">
         <h1>Home</h1>
-        <p> halaman ini merupakan sebuah halaman home dalam website Bukuku
+        <p>
         Selamat datang di Beranda Bukuku, tempat di mana petualangan literer Anda dimulai. Beranda Bukuku adalah jendela yang membuka dunia luas dari kata-kata, cerita, dan pengetahuan. Dibuat dengan perhatian pada detail dan kebutuhan pembaca modern, Beranda Bukuku adalah pusat informasi dan inspirasi untuk semua hal berkaitan dengan dunia buku.
         </p>
     </body>
